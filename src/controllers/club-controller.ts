9e4f8bebdef2c1d64151eece0ce81fadc784794b
ddunn11 <148179140+ddunn11@users.noreply.github.com>
2023-12-30 17:42:55 -0800

    set up barebones club-controller and meetings-controller

--- src/controllers/club-controller.ts
+++ src/controllers/club-controller.ts
@@ -1,1 +1,11 @@
-// const knex = require("knex")(require("../knexfile"));
+import dotenv from "dotenv";
+import _knex from "knex";
+import { knexConfig } from "../../knexfile";
+
+dotenv.config();
+const nodeProfile = process.env.NODE_PROFILE;
+const config =
+  nodeProfile === "development"
+    ? knexConfig.development
+    : knexConfig.production;
+const knex = _knex(config);
